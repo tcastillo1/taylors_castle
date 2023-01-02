f52a9a4b123a561296bf2d9307a4907b60f337e7
Taylor <tcastillo1@gmail.com>
2023-01-02 14:37:03 -0800

    blah

--- CS50/Lecture_8_oop/student.py
+++ CS50/Lecture_8_oop/student.py
@@ -1,17 +1,40 @@
 class Student:
-    ...
+    def __init__(self, name, house, patronas):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"]:
+            raise ValueError("Missing or invalid house")
+        self.name = name
+        self.house = house
+        self.patronas = patronas
+
+    def __str__(self):
+        return f"{self.name} from house {self.house}"
+
+    def charm(self):
+        match self.patronas:
+            case "Stag":
+                return "🦌"
+            case "Otter":
+                return "🦫"
+            case "Jack Russel terrier":
+                return "🐶"
+            case _:
+                return "🪄"
 
 
 def main():
-    student = get_student()
-    print(f"{student.name} from {student.house}")
+    testobject = get_student()
+    # print(f"{testobject.name} from house {testobject.house}")
+    print(testobject)
+    print(testobject.charm())
 
 
 def get_student():
-    student = Student()
-    student.name = input("name: ")
-    student.house = input("house: ")
-    return student
+    name = input("name: ")
+    house = input("house: ")
+    patronas = input("patronas: ")
+    return Student(name, house, patronas)
 
 
 if __name__ == "__main__":
